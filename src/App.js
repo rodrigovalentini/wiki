927694833163d13b9802f52d34d621de1e02430e
Rodrigo Valentini <rodrigo.valentini@outlook.com>
2020-01-08 18:09:22 -0300

    Login e register pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path="/login" component={Login}></Route>
+        <Redirect exact from="/" to="/login" />
+        <Route exact path="/register" component={Register}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/login/index.jsx
@@ -0,0 +1,110 @@
+import React, { Component, Fragment } from "react";
+import { Route, Link } from "react-router-dom";
+import Button from "@atlaskit/button";
+import Form, {
+  Field,
+  ErrorMessage,
+  FormFooter,
+  HelperMessage
+} from "@atlaskit/form";
+import TextField from "@atlaskit/textfield";
+import FieldText, { FieldTextStateless } from "@atlaskit/field-text";
+import "./style.css";
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  onChange = event => {
+    console.log(this.state);
+    this.setState({
+      eventResult: `onChange called with value: ${event.target.value}`
+    });
+  };
+
+  onBlur = () => {
+    this.setState({
+      eventResult: "onBlur called from FieldBase above"
+    });
+  };
+
+  onFocus = () => {
+    this.setState({
+      eventResult: "onFocus called from FieldBase above"
+    });
+  };
+
+  login = async data => {
+    await sleep(500);
+    const errors = {
+      email: !data.email.includes("@")
+        ? "Seu e-mail não é válido. Tente novamente!"
+        : undefined
+    };
+    if (!errors.username && !errors.email) {
+      console.log(data);
+    }
+    return errors;
+  };
+
+  handleSubmit = data => {
+    return this.login(data);
+  };
+  render() {
+    return (
+      <div
+        className="login"
+        style={{
+          display: "flex",
+          width: "400px",
+          maxWidth: "100%",
+          margin: "0 auto",
+          flexDirection: "column"
+        }}
+      >
+        <h1>Login</h1>
+        <Form onSubmit={this.handleSubmit}>
+          {({ formProps, dirty, submitting }) => (
+            <form {...formProps}>
+              <Field name="email" label="Email" defaultValue="" isRequired>
+                {({ fieldProps, error }) => (
+                  <Fragment>
+                    <TextField {...fieldProps} />
+                    {!error && <HelperMessage>Deve contar @</HelperMessage>}
+                    {error && <ErrorMessage>{error}</ErrorMessage>}
+                  </Fragment>
+                )}
+              </Field>
+              <Field
+                name="password"
+                defaultValue=""
+                label="Digite sua senha"
+                isRequired
+                type="password"
+              >
+                {({ fieldProps, error, valid }) => (
+                  <TextField {...fieldProps} />
+                )}
+              </Field>
+              <FormFooter>
+                <Link to="/register">Cadastrar-se</Link>
+                <Button
+                  type="submit"
+                  appearance="primary"
+                  isDisabled={!dirty || submitting}
+                >
+                  Entrar
+                </Button>{" "}
+              </FormFooter>
+            </form>
+          )}
+        </Form>
+      </div>
+    );
+  }
+}
+export default Login;
